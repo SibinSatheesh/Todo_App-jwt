09b967fab4aada7f0fa293fb90b75901d5b322cf
Sibin <sibinsathish@gmail.com>
2021-05-27 11:53:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
- require('./admin');
 
-const Users=mongoose.model("admin")
-// const User1=mongoose.model("users")
 //Import Routes
 
 const authRoutes = require('./routes/auth');
@@ -16,9 +14,7 @@ dotenv.config();
 
 //connect to DB
 
-const mongoUri= "mongodb+srv://connectdb:Rhino94@cluster0.gewim.mongodb.net/jwt?retryWrites=true&w=majority"
-
-mongoose.connect(mongoUri, { 
+mongoose.connect(process.env.MONGO_URI, { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true },
@@ -31,56 +27,8 @@ app.use(express.json());
 //Route Middlewares
 
 app.use('/api/user', authRoutes);
-app.use( '/api', postRoutes);
-
-
-
-app.get('/get',async  (req,res)=>{
-    await Users.find({})
-  .then(data=>{
-   res.send(data);  
-}).catch(err=>{
-console.log(err)
-})
-})
-
-app.post('/send', async (req,res)=>{
-    const user = new Users({
-        name: req.body.name,
-        position: req.body.position,
-        email: req.body.email,
-        phone: req.body.phone,
-    })
-    const savedPost = await user.save()
-    .then(data=>{console.log(data)
-        res.send(data); 
-}).catch(err=>{
-console.log(err)
-})
-})
-
-app.post('/delete',async  (req,res)=>{
-    await Users.findByIdAndRemove(req.body.id)
-  .then(data=>{console.log(data)
-   res.send(data);  
-}).catch(err=>{
-console.log(err)
-})
-})
-
-app.post('/update',async  (req,res)=>{
-    await Users.findByIdAndUpdate(req.body.id,
-        {
-            name: req.body.name,  
-            position: req.body.position,
-            email: req.body.email,
-            phone: req.body.phone,
-        })
-  .then(data=>{console.log(data)
-   res.send(data);  
-}).catch(err=>{
-console.log(err)
-})
-})
+app.use( '/', postRoutes);
 
 app.listen(3000,() => console.log('server running'));
+
+
